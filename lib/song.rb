479e6e4db49bc7cff19de84f9a45b1a4139a3553
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-31 06:05:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,25 +3,25 @@ class Song
   @@count = 0
   @@genres = []
   @@artists = []
-  
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
     @@genres << genre
-    @@artists << artist 
+    @@artists << artist
   end
 
   def self.count
     @@count
   end
 
-  def self.genre
-  @@genre
+  def self.genres
+  @@genres
   end
 
   def self.artists
-    @@artists 
+    @@artists
 
 end
