6c5ea0fa57dab4732c5cb447d48c1a5b9cdc35c6
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-31 06:02:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,22 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
+  @@genre =[]
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genre << genre 
   end
 
   def self.count
     @@count
   end
+
+  def self.genre
+  @@genre 
+  end 
 
 end
